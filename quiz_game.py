341ccee4a56a3f148f4e0fc232d119c737037c20
Abdi Deresa <Abdishegute@gmail.com>
2024-08-19 19:13:11 -0700

    added dictionary and a 2d list

--- quiz_game.py
+++ quiz_game.py
@@ -19,3 +19,10 @@ questions = {
     "Python is tributed to which comedy group? ": "C",
     "Is the Earth round?: ": "A"
 }
+
+options = [
+    ["A.Guido van Rossum", "B.Elon Musk", "C. Bill Gates", "D.Mark Zuckerburg"],
+    ["A. 1989","B. 1991","C. 2000", "D. 2016"],
+    ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+    ["A. True", "B. False", "C. sometimes", "D. what's Earth?"]
+]
